90f7563ddf55bc141b9fa0f4aeefd9b97bc9fa79
githubnzx <n_zhenxiang@163.com>
2019-02-26 17:22:10 +0800

    修改

--- application/common/config/DriverConfig.php
+++ application/common/config/DriverConfig.php
@@ -50,12 +50,12 @@ class DriverConfig extends BaseConfig
     public function truckPrice()
     {
         return [
-            ['id' => 1, 'starting_price' => "35", "excess_fee" => "3"], // 小型面包车
-            ['id' => 2, 'starting_price' => "49", "excess_fee" => "4"], // 小型平板车
-            ['id' => 3, 'starting_price' => "49", "excess_fee" => "4"], // 小型厢式货车
-            ['id' => 4, 'starting_price' => "95", "excess_fee" => "5"], // 中型厢式货车
-            ['id' => 5, 'starting_price' => "95", "excess_fee" => "5"], // 中型平板车
-            ['id' => 6, 'starting_price' => "95", "excess_fee" => "5"], // 中型高栏
+            ['id' => 1, 'starting_price' => "25", "excess_fee" => "3"], // 小型面包车
+            ['id' => 2, 'starting_price' => "40", "excess_fee" => "4"], // 小型平板车
+            ['id' => 3, 'starting_price' => "40", "excess_fee" => "4"], // 小型厢式货车
+            ['id' => 4, 'starting_price' => "90", "excess_fee" => "5"], // 中型厢式货车
+            ['id' => 5, 'starting_price' => "90", "excess_fee" => "5"], // 中型平板车
+            ['id' => 6, 'starting_price' => "90", "excess_fee" => "5"], // 中型高栏
         ];
     }
 
